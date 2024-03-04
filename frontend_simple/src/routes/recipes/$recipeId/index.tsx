a38f2dc3df4192b99ef6137f1082438dc1ab933b
Nils Hartmann <nils@nilshartmann.net>
2024-03-04 18:38:54 +0100

    refactoring #2

--- frontend_simple/src/routes/recipes/$recipeId/index.tsx
+++ frontend_simple/src/routes/recipes/$recipeId/index.tsx
@@ -1,7 +1,17 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { useGetRecipeQuery } from "../../../components/use-queries.ts";
-import { RecipePageContent } from "./-components/RecipePageContent.tsx";
 import z from "zod";
+import { RecipeBanner } from "./-components/RecipeBanner.tsx";
+import { CookingTime } from "./-components/CookingTime.tsx";
+import { Ingredients } from "./-components/Ingredients.tsx";
+import { Instructions } from "./-components/Instructions.tsx";
+import { Sidebar } from "../../../components/Sidebar.tsx";
+import { H2 } from "../../../components/Heading.tsx";
+import React, { Suspense } from "react";
+import LoadingIndicator from "../../../components/LoadingIndicator.tsx";
+import FeedbackList from "./-components/FeedbackList.tsx";
+import "./-components/RecipePage.css";
+import { useRecipifyWindowTitle } from "../../../components/useRecipifyWindowTitle.tsx";
 
 const RecipePageParams = z.object({
   feedback_page: z.number().optional().default(0),
@@ -15,6 +25,37 @@ export const Route = createFileRoute("/recipes/$recipeId/")({
 function RecipePage() {
   const { recipeId } = Route.useParams();
   const { data } = useGetRecipeQuery(recipeId);
+  const recipe = data.recipe;
 
-  return <RecipePageContent recipe={data.recipe} />;
+  useRecipifyWindowTitle(recipe.title);
+
+  return (
+    <div className={"mb-20"}>
+      <RecipeBanner recipe={recipe} />
+      <div className={"container mx-auto mb-8 mt-8 md:flex md:space-x-12"}>
+        <div className={"md:w-2/3"}>
+          <CookingTime
+            cookTime={recipe.cookTime}
+            preparationTime={recipe.preparationTime}
+          />
+          <Ingredients recipe={recipe} />
+          <Instructions recipe={recipe} />
+        </div>
+        <div className={"md:w-1/3"}>
+          <Sidebar>
+            <H2>Feedback</H2>
+            <Suspense
+              fallback={
+                <LoadingIndicator>Loading feedback...</LoadingIndicator>
+              }
+            >
+              <FeedbackList recipeId={recipe.id} />
+            </Suspense>
+          </Sidebar>
+        </div>
+      </div>
+    </div>
+  );
+
+  // return <RecipePageContent recipe={data.recipe} />;
 }
